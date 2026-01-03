eccb3bd11d6b8ebb4eafe1479916e7a12147943a
Bibek Tamang <bibek7here@gmail.com>
2026-01-03 17:46:41 +0000

    peer init

--- client/main.go
+++ client/main.go
@@ -9,12 +9,13 @@ import (
 
 func main() {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr: "localhost:6379",
 	})
 	ctx := context.Background()
 
-	err := client.Set(ctx, "what", "world", 0).Err()
+	err := client.Set(ctx, "hello", 1, 0).Err()
 	if err != nil {
 		log.Fatal("ERROR: ", err)
 	}
+
 }
--- internals/server.go
+++ internals/server.go
@@ -2,17 +2,41 @@ package internals
 
 import (
 	"fmt"
+	"log/slog"
 	"net"
-	"strings"
 )
 
-type Server struct {
+type Config struct {
 	Addr string
 }
 
-func NewServer(addr string) *Server {
+type Server struct {
+	Config
+	Listener   net.Listener
+	peers      map[*Peer]bool
+	addPeer    chan *Peer
+	deletePeer chan *Peer
+	quitCh     chan struct{}
+}
+
+func NewServer(config Config) *Server {
 	return &Server{
-		Addr: addr,
+		Config:     config,
+		peers:      make(map[*Peer]bool),
+		addPeer:    make(chan *Peer),
+		deletePeer: make(chan *Peer),
+		quitCh:     make(chan struct{}),
+	}
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case p := <-s.addPeer:
+			s.peers[p] = true
+		case <-s.quitCh:
+			return
+		}
 	}
 }
 
@@ -21,9 +45,12 @@ func (s *Server) ListenAndServer() error {
 	if err != nil {
 		return err
 	}
+	s.Listener = listener
+
+	go s.loop()
+
 	fmt.Println("Server is running at PORT ", s.Addr)
 	for {
-
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
@@ -36,32 +63,10 @@ func (s *Server) ListenAndServer() error {
 
 func (s *Server) handleConnection(conn net.Conn) {
 	defer conn.Close()
-	for {
-		buf := make([]byte, 1024)
-		n, err := conn.Read(buf)
-		if err != nil {
-			return
-		}
+	peer := NewPeer(conn)
 
-		data := string(buf[:n])
-		commands := strings.Split(data, "*")
-
-		for _, cmd := range commands {
-			if cmd == "" {
-				continue
-			}
-
-			fmt.Printf("Processing Command: *%s\n", strings.ReplaceAll(cmd, "\r\n", " "))
-
-			if strings.Contains(cmd, "hello") {
-				conn.Write([]byte("%3\r\n$6\r\nserver\r\n$15\r\nmy-custom-redis\r\n$7\r\nversion\r\n$5\r\n1.0.0\r\n$5\r\nproto\r\n:3\r\n"))
-			} else if strings.Contains(cmd, "client") {
-				conn.Write([]byte("+OK\r\n"))
-			} else if strings.Contains(cmd, "set") {
-				fmt.Println("--- RECEIVED THE SET COMMAND! ---")
-				fmt.Println("data: ", cmd)
-				conn.Write([]byte("+OK\r\n"))
-			}
-		}
+	s.addPeer <- peer
+	if err := peer.readLoop(); err != nil {
+		slog.Error("peer read error", "err", err, "remote Addr", conn.RemoteAddr().String())
 	}
 }

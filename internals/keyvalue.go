1af3746d43960aee45f4b67a5bef30a1ef9641e6
Bibek Tamang <bibek7here@gmail.com>
2026-01-04 13:28:57 +0000

    init: keyvalue

---
+++ internals/keyvalue.go
@@ -0,0 +1,28 @@
+package internals
+
+import "sync"
+
+type KeyValue struct {
+	mu   sync.RWMutex
+	Data map[string][]byte
+}
+
+func NewKeyValue() *KeyValue {
+	return &KeyValue{}
+}
+
+func (kv *KeyValue) Set(key, value []byte) error {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.Data[string(key)] = value
+	return nil
+}
+
+func (kv *KeyValue) Get(key []byte) ([]byte, bool) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	val, ok := kv.Data[string(key)]
+	return val, ok
+
+}

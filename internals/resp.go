2e4d0a8ba3d8cec0346dfe4c71256115d0b955bd
Bibek Tamang <bibek7here@gmail.com>
2026-01-04 12:52:03 +0000

    feat: RESP WRITE

--- internals/resp.go
+++ internals/resp.go
@@ -22,6 +22,61 @@ type Value struct {
 	bulk     string
 	array    []Value
 }
+func (v Value) Marshal() []byte{
+	switch v.typ {
+	case "array":
+	}
+}
+
+func (v Value) marshalString() []byte{
+	var bytes []byte
+
+	bytes = append(bytes, STRING)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r','\n')
+
+	return byte
+}
+
+func (v value) marshalBulk()[]byte {
+	var bytes []byte
+
+	bytes = append(bytes, BULK)
+	bytes = append(bytes, strconv.Itoa(len(v.bulk))...)
+	bytes = append(bytes, '\r', '\n')
+	bytes = append(bytes, v.bulk...)
+	bytes = append(bytes, '\r', '\n')
+
+	return bytes
+}
+
+func (v Value) marshalArray() []byte {
+	len := len(v.array)
+	var bytes []byte
+
+	bytes = append(bytes, ARRAY)
+	bytes =append(bytes, strconv.Itoa(len)...)
+	bytes = append(bytes, '\r', '\n')
+
+	for i := 0; i < len; i++ {
+		bytes = append(bytes, v.array[i].Marshal()...)
+	}
+
+	return bytes
+}
+
+func (v Value) marshallError() []byte {
+	var bytes []byte
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+
+	return bytes
+}
+
+func (v Value) marshallNull() []byte {
+	return []byte("$-1\r\n")
+}
 
 type Resp struct {
 	// Handshake
@@ -123,4 +178,20 @@ func (r *Resp) readBulk() (Value, error) {
 
 	return v, nil
 }
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{
+		writer: w,
+	}
+}
+
+func (w *Writer) Write() error {
+	var bytes = v.
+	return nil
+}
+
 
